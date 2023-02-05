78ab8d0b5420715759ba69b4acbba559b96cf5c0
Hunter Association <109690253+HunterAssociation@users.noreply.github.com>
2023-02-06 01:42:28 +0700

    Update helpers.py

--- strings/helpers.py
+++ strings/helpers.py
@@ -177,3 +177,25 @@ HELP_5 = """🔰**<u>ADD & REMOVE SUDO USERS :</u>**
 **Exemplo:** `/broadcast -user -assistant -pin Boe die!`
 
 """
+
+HELP_7 = """
+##############################
+#        **BOT COMMANDS**        #
+##############################
+
+**For Users :**
+<code>/play</code> - To play music.
+<code>/queue</code> - To see queues.
+
+**For Admins :**
+<code>/end</code> - To end the song.
+<code>/skip</code> - To skip the song.
+<code>/pause</code> - To pause the song.
+<code>/resume</code> - To resume the song.
+<code>/mute</code> - To mute the song.
+<code>/unmute</code> - To unmute the song.
+"""
+
+
+
+
