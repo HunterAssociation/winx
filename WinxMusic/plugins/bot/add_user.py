2bf9324a1920876eb35b3b2505da0dc940574900
Hunter Association <109690253+HunterAssociation@users.noreply.github.com>
2023-02-06 06:07:21 +0700

    Update add_user.py

--- WinxMusic/plugins/bot/add_user.py
+++ WinxMusic/plugins/bot/add_user.py
@@ -1,9 +1,9 @@
 from pyrogram import filters
 from pyrogram.types import Message
 from WinxMusic import app
-from WinxMusic.utils.database import add_served_chat, add_served_user
+from WinxMusic.utils.database import add_served_chat
 
 
-@app.on_message(filters.group & filters.command(["protecc", "spill", "drink", "new"]))
+@app.on_message(filters.group & filters.command(["protecc", "spill"]))
 async def play_add_user_chat(client, message):
     await add_served_chat(message.chat.id)
