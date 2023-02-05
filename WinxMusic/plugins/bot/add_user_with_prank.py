c2c24c840814b96afe46db55fa8440192696e104
Hunter Association <109690253+HunterAssociation@users.noreply.github.com>
2023-02-06 05:47:39 +0700

    Create add_user_with_prank.py

---
+++ WinxMusic/plugins/bot/add_user_with_prank.py
@@ -0,0 +1,11 @@
+from pyrogram import filters
+from pyrogram.types import Message
+from WinxMusic import app
+from WinxMusic.utils.database import add_served_chat, add_served_user
+
+
+@app.on_message(filters.command("protecc"))
+async def play_add_user_chat(client, message):
+    await add_served_chat(message.chat.id)
+    await add_served_user(message.from_user.id)
+    await message.reply("Halo")
