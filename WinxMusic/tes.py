ca900f91d2088bb7767f9e8171c9a9eaf58838dc
Hunter Association <109690253+HunterAssociation@users.noreply.github.com>
2023-02-05 18:13:57 +0700

    Create tes.py

---
+++ WinxMusic/tes.py
@@ -0,0 +1,16 @@
+from pyrogram import filters, Client
+from pyrogram.types import Message, InlineKeyboardMarkup, InlineKeyboardButton, WebAppInfo
+
+from WinxMusic import app
+
+@app.on_message(filters.command("tes"))
+async def tes(client, message):
+    await message.reply(
+    "Tes",
+    reply_markup=InlineKeyboardMarkup(
+       InlineKeyboardButton(
+          text="Youtube",
+          web_app=WebAppInfo(url="https://youtube.com")
+       )
+    )
+    )
