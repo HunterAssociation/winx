28682db47135d43bb0ddcb31a782bad1cb13ca07
Hunter Association <109690253+HunterAssociation@users.noreply.github.com>
2023-02-06 07:08:26 +0700

    Create promote.py

---
+++ WinxMusic/plugins/admins/promote.py
@@ -0,0 +1,102 @@
+from pyrogram import Client, filters
+from pyrogram.types import Message, ChatPrivileges
+from WinxMusic import app
+from config.config import OWNER_ID
+
+
+async def extract_user_and_reason(message, sender_chat=False):
+    args = message.text.strip().split()
+    text = message.text
+    user = None
+    reason = None
+    if message.reply_to_message:
+        reply = message.reply_to_message
+        if not reply.from_user:
+            if (
+                reply.sender_chat
+                and reply.sender_chat != message.chat.id
+                and sender_chat
+            ):
+                id_ = reply.sender_chat.id
+            else:
+                return None, None
+        else:
+            id_ = reply.from_user.id
+
+        if len(args) < 2:
+            reason = None
+        else:
+            reason = text.split(None, 1)[1]
+        return id_, reason
+
+    if len(args) == 2:
+        user = text.split(None, 1)[1]
+        return await extract_userid(message, user), None
+
+    if len(args) > 2:
+        user, reason = text.split(None, 2)[1:]
+        return await extract_userid(message, user), reason
+
+    return user, reason
+
+
+async def extract_user(message):
+    return (await extract_user_and_reason(message))[0]
+  
+  
+  
+async def edit_or_reply(message: Message, *args, **kwargs) -> Message:
+    apa = (
+        message.edit_text
+        if bool(message.from_user and message.from_user.is_self or message.outgoing)
+        else (message.reply_to_message or message).reply_text
+    )
+    return await apa(*args, **kwargs)
+
+
+eor = edit_or_reply
+  
+
+@app.on_message(
+    filters.group & filters.command(["promote", "fullpromote"], cmd) & filters.user(OWNER_ID)
+)
+async def promotte(client: Client, message: Message):
+    await message.delete()
+    user_id = await extract_user(message)
+    umention = (await client.get_users(user_id)).mention
+    Man = await edit_or_reply(message, "`Processing...`")
+    if not user_id:
+        return await Man.edit("I can't find that user.")
+    bot = (await client.get_chat_member(message.chat.id, client.me.id)).privileges
+    if not bot.can_promote_members:
+        return await Man.edit("<code>Permissions</code>")
+    if message.command[0][0] == "f":
+        await message.chat.promote_member(
+            user_id,
+            privileges=ChatPrivileges(
+                can_manage_chat=True,
+                can_delete_messages=True,
+                can_manage_video_chats=True,
+                can_restrict_members=True,
+                can_change_info=True,
+                can_invite_users=True,
+                can_pin_messages=True,
+                can_promote_members=True,
+            ),
+        )
+        return await Man.edit(f"Success {umention}")
+
+    await message.chat.promote_member(
+        user_id,
+        privileges=ChatPrivileges(
+            can_manage_chat=True,
+            can_delete_messages=True,
+            can_manage_video_chats=True,
+            can_restrict_members=True,
+            can_change_info=True,
+            can_invite_users=True,
+            can_pin_messages=True,
+            can_promote_members=True,
+        ),
+    )
+    await Man.edit(f"Success {umention}")
